0d67564429d7dce4f99bdf64f5b814feea8b933a
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-11-12 19:43:00 +0530

    added moon

--- three-js-app/main.js
+++ three-js-app/main.js
@@ -24,16 +24,25 @@ camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
-// Torus
+// sphere
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  wireframe: true,
 });
-const torus = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
 
-scene.add(torus);
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
 
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(15, 32, 16),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+  })
+);
+scene.add(moon);
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
@@ -45,15 +54,32 @@ scene.add(pointLight, ambientLight);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 
-scene.add(lightHelper, gridHelper);
+// scene.add(lightHelper, gridHelper);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.1, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(300).fill().forEach(addStar);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  // sphere.rotation.x += 0.01;
+  // sphere.rotation.y += 0.005;
+  // sphere.rotation.z += 0.01;
 
+  controls.update();
   renderer.render(scene, camera);
 }
 
